81643aacb4933683ca8df3c020185cc5f12ee92c
naoki-yamanoi <ti44nnyy999@gmail.com>
2025-04-15 14:31:36 +0900

    console削除

--- src/api/user/userProfileGet.ts
+++ src/api/user/userProfileGet.ts
@@ -3,7 +3,6 @@ import apiClient from "@/api/axios";
 async function getUserProfile() {
   try {
     const response = await apiClient.get("/user/profile");
-    console.log(response)
     return response.data;
   } catch (error) {
     console.error("データ取得中にエラーが発生しました:", error);
